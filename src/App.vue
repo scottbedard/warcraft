6d5f0fa4108a811df14ebb5b151412eb1bf54f6b
Scott Bedard <scottbedard90@gmail.com>
2024-05-17 15:16:27 -0500

    setup canvas

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    Check back later
+    <Application :resize-to="win">
+      <container>
+        <graphics
+          :x="120"
+          :y="120"
+          @render="graphics => {
+            graphics.clear()
+            graphics.beginFill(0xDE3249)
+            graphics.drawCircle(0, 0, 50)
+            graphics.endFill()
+          }"
+        />
+      </container>
+    </Application>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Application } from 'vue3-pixi';
+
+const win = window
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { compilerOptions, transformAssetUrls } from 'vue3-pixi'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions, // <- support custom elements without warning
+        transformAssetUrls, // <- support for asset url conversion
+      },
+    }),
+  ],
 })
