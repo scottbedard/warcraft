29bdc0df1d9cfce8e18bbd78715f09a2e436cada
Scott Bedard <scottbedard90@gmail.com>
2024-05-17 15:13:07 -0500

    tailwind + pixi

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{ts,vue}',
+  ],
+  plugins: [],
+  theme: {
+    extend: {},
+  },
+}
+
